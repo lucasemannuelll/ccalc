726c832cb6cb8f1dffa9e672aebe82ac59fccc29
lucasemannuelll <163750716+lucasemannuelll@users.noreply.github.com>
2026-05-25 15:50:41 -0300

    Rename read_input to ler_entrada in entrada.h

---
+++ src/entrada.h
@@ -0,0 +1,6 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+int ler_entrada(double *valor_a, double *valor_b, char *operador); 
+
+#endif
--- src/input.h
+++
@@ -1,6 +0,0 @@
-#ifndef INPUT_H
-#define INPUT_H
-
-int read_input(double *a, double *b, char *op); 
-
-#endif
